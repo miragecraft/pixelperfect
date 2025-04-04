53a65634535173f734444703fd9f9be0a47ffa83
Shaw Jia <xiaozj@gmail.com>
2025-04-04 13:27:18 -0400

    Add files via upload
    v1.1

--- pixelperfect.js
+++ pixelperfect.js
@@ -1,4 +1,4 @@
-/*  PixelPerfect
+/*  PixelPerfect v1.1
     http://www.miragecraft.com
     Licensed under MIT Open Source
 
@@ -22,46 +22,62 @@ match.stop();
 match.restore();
 
 // recalculate
-match.recalc();
+match.update();
 */
 
-function pixelPerfect(props, el=document.documentElement) {
-
+function pixelPerfect(props, el = document.documentElement) {
   if (typeof props === 'string') props = [props];
   if (typeof el === 'string') el = document.querySelector(el);
 
-  props = new Map(props.map((p)=>[p,getComputedStyle(el).getPropertyValue(p)]));
+  let mediaQuery = null;
 
-  let mql;
+  const reset = () => props.forEach(prop => el.style.removeProperty(prop));
 
-  matchMedia();
+  const update = () => {
+    const ratio = window.devicePixelRatio;
+    reset();
+    const computed = getComputedStyle(el);
+    props.forEach(prop => {
+      const val = parseInt(computed.getPropertyValue(prop), 10);
+      if (!isNaN(val)) {
+        el.style.setProperty(prop, `${Math.round(val * ratio) / ratio}px`);
+      } else {
+        console.warn(`Warning: The value for property "${prop}" is not a valid number.`);
+      }
+    });
+  };
 
-  function matchMedia() {
+  const detach = () => {
+    if (mediaQuery) {
+      mediaQuery.removeEventListener('change', refresh);
+      mediaQuery = null;
+    }
+  };
 
-    let ratio = window.devicePixelRatio;
+  const attach = () => {
+    const ratio = window.devicePixelRatio;
+    mediaQuery = window.matchMedia(`not screen and (resolution: ${ratio}dppx)`);
+    mediaQuery.addEventListener('change', refresh);
+  };
 
-    props.forEach((val,prop)=>{
-      el.style.setProperty(prop, Math.round(parseInt(val)*ratio)/ratio + 'px'); 
-    })
+  // On resolution change, update and rebind the listener.
+  const refresh = () => {
+    detach();
+    update();
+    attach();
+  };
 
-    mql = window.matchMedia('not screen and (resolution: '+ratio+'dppx)');
-    mql.addEventListener('change', matchMedia, {once:true});
-  }
+  // Initial update and listener attachment.
+  update();
+  attach();
 
   return {
-    stop: ()=>{
-      mql.removeEventListener('change', matchMedia, {once:true});
+    start: refresh,
+    stop: detach,
+    restore: () => {
+      detach();
+      reset();
     },
-    restore: function () {
-      this.stop;
-      props.forEach((val,prop)=>{
-        el.style.removeProperty(prop)
-      })
-    },
-    recalc: function () {
-      this.restore();
-      matchMedia();    
-    }
-  }
-
+    update
+  };
 }
